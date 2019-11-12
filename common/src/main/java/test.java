1d4d74ff410e94fa6a9cbaee362836d697c9c7df
Username <Username@example.com>
2019-11-12 11:47:31 +0800

    第一次提交，yzy

---
+++ common/src/main/java/test.java
@@ -0,0 +1,13 @@
+/**
+ * @program: jinxiuxiangqing
+ * @author: yzy
+ * @create: 2019-11-12 11:46
+ **/
+
+/**
+ *@ClassName test
+ *@Author yzy
+ *@Date 2019/11/12 11:46
+ */
+public class test {
+}
---
+++ springcloud-api/src/main/java/testService.java
@@ -0,0 +1,7 @@
+/**
+ * @program: jinxiuxiangqing
+ * @author: yzy
+ * @create: 2019-11-12 11:47
+ **/
+public interface testService {
+}
